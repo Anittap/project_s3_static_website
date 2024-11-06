9e724fecf65c4e21a8756e177befa56ed5baf92e
anitta <anittaphilip001@gmail.com>
2024-11-06 20:14:45 +0000

    Created Route table subnet association

--- main.tf
+++ main.tf
@@ -61,4 +61,13 @@ resource "aws_route_table" "private" {
     Name = "${var.project}-${var.environment}"
   }
 }
-
+resource "aws_route_table_association" "public" {
+  count          = 3
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private" {
+  count          = 3
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
