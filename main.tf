53fc39015e60d9873b83d422e0a9d2ae14683d44
anitta <anittaphilip001@gmail.com>
2024-11-06 20:20:46 +0000

    Added IGW route

--- main.tf
+++ main.tf
@@ -71,3 +71,9 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
+resource "aws_route" "igw" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
