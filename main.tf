181f70be8d631a56ebf7e3d7f93284e715b81fed
anitta <anittaphilip001@gmail.com>
2024-11-06 20:25:07 +0000

    Added NAT GW route

--- main.tf
+++ main.tf
@@ -76,4 +76,9 @@ resource "aws_route" "igw" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
+resource "aws_route" "nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
 
