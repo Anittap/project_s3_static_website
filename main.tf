11f5468d6cd2c23dde8649553002b1c75412c8e8
anitta <anittaphilip001@gmail.com>
2024-11-09 16:55:29 +0000

    Allowed all outgoing traffic on lb security group

--- main.tf
+++ main.tf
@@ -109,3 +109,8 @@ resource "aws_vpc_security_group_ingress_rule" "lb" {
   ip_protocol       = "tcp"
   to_port           = each.key
 }
+resource "aws_vpc_security_group_egress_rule" "lb" {
+  security_group_id = aws_security_group.lb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
