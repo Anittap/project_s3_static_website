44f9cb0444c093f037347db1f28ccfb53eefe272
anitta <anittaphilip001@gmail.com>
2025-01-09 23:33:05 +0000

    Defined the template_file data source to read the IAM policy document

--- terraform/datasource.tf
+++ terraform/datasource.tf
@@ -2,3 +2,11 @@ data "aws_route53_zone" "my_domain" {
   name         = var.domain_name
   private_zone = false
 }
+data "template_file" "s3_public_policy" {
+
+  template = file("${path.module}/s3_public_policy.json")
+
+  vars = {
+    bucket_name = "${var.environment}.${var.domain_name}"
+  }
+}
