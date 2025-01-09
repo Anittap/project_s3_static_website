64d2cb1db3d4336bccf47c1fd2143e572ab1a0e0
anitta <anittaphilip001@gmail.com>
2025-01-09 23:39:14 +0000

    Disabled Block all public access

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,11 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 }
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
