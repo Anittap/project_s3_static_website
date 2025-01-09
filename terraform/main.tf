fe5b4b9f35631ab526044d601475784dc58a921a
anitta <anittaphilip001@gmail.com>
2025-01-09 23:27:28 +0000

    Enabled Bucket Website Hosting

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,14 @@ resource "aws_s3_bucket" "website" {
     Name = "${var.project}-${var.environment}-bucket"
   }
 }
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
