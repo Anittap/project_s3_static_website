315bddad7df52a1bfe009188797ffccf8253a871
anitta <anittaphilip001@gmail.com>
2025-01-10 00:29:53 +0000

    Removed region variable

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,4 +1,3 @@
-region      = "us-east-1"
 environment = "development"
 project     = "project_s3_static_website"
 domain_name = "anitta.cloud"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,3 @@
-variable "region" {
-  type        = string
-  description = "aws region name"
-}
 variable "environment" {
   type        = string
   description = "project environment name"
